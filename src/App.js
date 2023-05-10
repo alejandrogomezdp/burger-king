9a72ba9a976519877701bb287825203e07a5c41d
alejandrogomezdp <alejandrogomezdp@gmail.com>
2023-05-10 12:25:09 +0200

    plantilla nueva

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
-import Title from "./components/HeaderTitle";
-import Paragraph from "./components/FillerParagraph";
+// import Title from "./components/HeaderTitle";
+// import Paragraph from "./components/FillerParagraph";
 
 const App = () => {
   return (
     <div className="main">
-      <Title />
-      <Paragraph />
-      <Paragraph />
-      <Paragraph />
+      <div className="circulo"></div>
+      <img className="foto1" src="https://drscdn.500px.org/photo/88195029/m%3D900/v2?sig=727ce32f415cfbee114e39912e6513685783723e9c7c91e5bfe2110891c12a83" alt="Italian Trulli"></img>
+      <h2 className="foto1nombre">Dr. Widowoman</h2>
+      <p className="foto1trabajo">Project Manager</p>
     </div>
   );
 };
